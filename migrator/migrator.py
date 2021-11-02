ffeca5e0374590cc9e54340a932f0b8cfc2a99ac
ronnifadhlillah <33772449+ronnifadhlillah@users.noreply.github.com>
2021-11-02 22:08:46 +0700

    sqlite file generator
    simple and short code to generate sqlite file.

--- migrator/migrator.py
+++ migrator/migrator.py
@@ -1,10 +1,28 @@
 import click
+import configparser
+import os
+import sys
+
+p=configparser.ConfigParser()
+p.read(os.getcwd()+'/migrator.conf')
+driver=p['database']['driver']
+
 
 @click.group()
 def main():
     """MIGRATOR : THE DATABASE MADE IN CLI"""
 
 @main.command()
-@click.option('--db', nargs=1, type=str)
+@click.option('--db',nargs=1,type=str,help='TEXT replace with database name.')
 def init(db):
-    click.echo("%s world" %db)
+    """Initialize new database"""
+    if driver == 'sqlite':
+        if os.path.isdir(p[driver]['path']) is False:
+            os.mkdir(p[driver]['path'],777)
+            os.chdir(p[driver]['path'])
+            wconfig=open(db+'.'+driver, "x")
+            wconfig.close()
+            print("""Database %s is successfully created. Check folder %s to view database file. """ % (db+'.'+driver,p[driver]['path']))
+        else:
+            print("""folder %s exists.""" % p[driver]['path'])
+            sys.exit()
